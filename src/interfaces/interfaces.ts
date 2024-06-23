eda68df34c6c942d4f5195634b3596a79d269369
Camilo Acuna <camilo.acuna@orbis-dev.cl>
2024-06-23 19:57:11 -0400

    add product interface

---
+++ src/interfaces/interfaces.ts
@@ -0,0 +1,11 @@
+export interface Product {
+  id: number;
+  description: string;
+  name: string;
+  stock: number;
+  author: string;
+  category: string;
+  price: number;
+  image: string;
+  cantidad?: number;
+}
