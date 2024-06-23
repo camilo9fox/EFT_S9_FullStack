5ab122980b96e67301ada04b82086510bcb7f120
Camilo Acuna <camilo.acuna@orbis-dev.cl>
2024-06-23 19:42:47 -0400

    configure main component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomePageComponent } from "./store/pages/HomePage/HomePage.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, HomePageComponent]
 })
 export class AppComponent {
   title = 'pepe-libreria';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './store/pages/HomePage/HomePage.component';
+import { StorePageComponent } from './store/pages/StorePage/StorePage.component';
+import { AboutUsPageComponent } from './store/pages/AboutUsPage/AboutUsPage.component';
+import { LoginPageComponent } from './login/pages/LoginPage/LoginPage.component';
+import { RegisterPageComponent } from './login/pages/RegisterPage/RegisterPage.component';
+import { RecoveryPasswordPageComponent } from './login/pages/RecoveryPasswordPage/RecoveryPasswordPage.component';
+import { AuthGuard } from './login/guards/Auth.guard';
+import { MyProfilePageComponent } from './store/pages/MyProfilePage/MyProfilePage.component';
+import { OrderComponent } from './store/pages/Order/Order.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'tienda', component: StorePageComponent },
+  { path: 'nosotros', component: AboutUsPageComponent },
+  { path: 'login', component: LoginPageComponent },
+  { path: 'registro', component: RegisterPageComponent },
+  { path: 'recuperar-contraseña', component: RecoveryPasswordPageComponent },
+  { path: 'pedido', component: OrderComponent },
+  {
+    path: 'mi-perfil',
+    component: MyProfilePageComponent,
+    canActivate: [AuthGuard],
+  },
+];
