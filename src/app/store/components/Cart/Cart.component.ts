168d63c56fa18f1fa0d70386a9f5d438a0a0ee71
Camilo Acuna <camilo.acuna@orbis-dev.cl>
2024-06-23 19:53:52 -0400

    add cart component

---
+++ src/app/store/components/Cart/Cart.component.ts
@@ -0,0 +1,56 @@
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { CartService } from '../../services/CartService.service';
+import { CommonModule } from '@angular/common';
+import { Product } from '../../../../interfaces/interfaces';
+
+/**
+ * @description
+ * Componente de la página "Carro de compras".
+ *
+ * Este componente está disponible de forma transversal en la plataforma,
+ * permitiendo agrupar y visualizar productos para su posterior compra.
+ */
+
+@Component({
+  selector: 'app-cart',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: 'Cart.component.html',
+  styleUrl: 'Cart.component.scss',
+})
+export class NameComponent implements OnInit {
+  cart: Product[] = [];
+  total = 0;
+  isOpen = false;
+
+  constructor(
+    private cartService: CartService,
+    private cdr: ChangeDetectorRef
+  ) {}
+
+  ngOnInit() {
+    this.cart = this.cartService.getCart();
+    this.total = this.cartService.updateCartItems();
+
+    this.cartService.isCartOpen$.subscribe((isOpen) => {
+      this.isOpen = isOpen;
+      this.cdr.markForCheck();
+    });
+
+    this.cartService.cartTotal$.subscribe((total) => {
+      this.total = total;
+      this.cdr.markForCheck();
+    });
+  }
+
+  removeFromCart(productId: number): void {
+    this.cartService.removeFromCart(productId);
+    this.cart = this.cartService.getCart();
+    this.total = this.cartService.updateCartItems();
+    this.cdr.markForCheck();
+  }
+
+  toggleCart(): void {
+    this.cartService.toggleCart();
+  }
+}
