3e5fdfb1a92c13b0308b61a425e0f843fd4a451a
Camilo Acuna <camilo.acuna@orbis-dev.cl>
2024-06-23 19:44:59 -0400

    add navbar component

---
+++ src/app/commons/components/Navbar/Navbar.component.ts
@@ -0,0 +1,67 @@
+import { CommonModule } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+  ChangeDetectorRef,
+} from '@angular/core';
+import { CartService } from '../../../store/services/CartService.service';
+import { SessionService } from '../../../login/services/session.service';
+import { ModalComponent } from '../Modal/Modal.component';
+import { Router } from '@angular/router';
+
+/**
+ * @description
+ * Componente de la barra de navegación ("Navbar").
+ *
+ * Este componente proporciona la barra de navegación principal para la plataforma.
+ * Incluye enlaces a las diferentes secciones del sitio web, como la página de inicio,
+ * tienda, carrito de compras, y secciones de autenticación (inicio de sesión y registro).
+ * También puede mostrar el estado de la sesión del usuario, permitiendo acceso rápido
+ * a la información de la cuenta y opciones de cierre de sesión.
+ */
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  templateUrl: './Navbar.component.html',
+  styleUrl: './Navbar.component.scss',
+  imports: [CommonModule, ModalComponent],
+})
+export class NavbarComponent implements OnInit {
+  cartItemCount = 0;
+  isLogged: boolean = false;
+  isModalOpen: boolean = false;
+
+  constructor(
+    private cartService: CartService,
+    private sessionService: SessionService,
+    private cdr: ChangeDetectorRef,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.cartService.cartItemCount$.subscribe((count) => {
+      this.cartItemCount = count;
+      this.cdr.markForCheck();
+    });
+    this.sessionService.isLogged$.subscribe((isLogged) => {
+      this.isLogged = isLogged;
+      this.cdr.markForCheck();
+    });
+    this.getStatusLogin();
+  }
+
+  openCart(): void {
+    this.cartService.openCart();
+  }
+
+  getStatusLogin() {
+    this.sessionService.getStatusLogin();
+  }
+
+  onCloseSession() {
+    this.sessionService.closeSession();
+    this.router.navigate(['/']);
+  }
+}
